b20e3d696e70aa4b0e0c28a4a77b301f68cfba23
noobapooo <apoorvtripathi99@gmail.com>
2024-06-10 06:49:47 +0530

    use products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,3 @@
-const products = [
-  {
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    rating: {
-      stars: 4.5,
-      counts: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    name: "Intermediate Size Basketball",
-    image: "images/products/intermediate-composite-basketball.jpg",
-    rating: {
-      stars: 4,
-      counts: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    rating: {
-      stars: 4.5,
-      counts: 56,
-    },
-    priceCents: 790,
-  },
-];
 let productsHTML = ``;
 products.forEach((product) => {
   productsHTML += `
